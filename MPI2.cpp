79bebf26ab6e9695c6915a9ef15671c8b9f56f53
Shunskiy <pas4o@yandex.ru>
2021-04-16 00:33:47 +0400

    Update MPI2.cpp

--- MPI2.cpp
+++ MPI2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(int argc, char** argv)
 {
     int rank, size;
-    int i, j, n = 1000;
+    int i, j, n = 10000;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
